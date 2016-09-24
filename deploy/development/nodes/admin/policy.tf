cb2d998ae3e98652b8d9a6b4338500e362c744a6
Paul Severance <pseverance@deepfield.net>
2016-09-23 22:09:46 -0400

    allow admin nodes to run inventory.py without credentials

--- deploy/development/nodes/admin/policy.tf
+++ deploy/development/nodes/admin/policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2" {
-  name = "${var.region}-${var.deploy}-ec2"
+  name = "${var.region}-${var.deploy}-${var.name}-ec2"
   path = "/instance/"
   assume_role_policy = <<EOF
 {
@@ -19,7 +19,7 @@ EOF
 }
 
 resource "aws_iam_policy" "ec2" {
-  name = "${var.region}-${var.deploy}-ec2"
+  name = "${var.region}-${var.deploy}-${var.name}-ec2"
   path = "/instance/"
   policy = <<EOF
 {
@@ -27,7 +27,7 @@ resource "aws_iam_policy" "ec2" {
   "Statement": [
     {
       "Action": [
-        "ec2:Describe*"
+        "ec2:Describe*",
       ],
       "Effect": "Allow",
       "Resource": "*"
@@ -38,12 +38,13 @@ EOF
 }
 
 resource "aws_iam_policy_attachment" "ec2" {
-  name = "${var.region}-${var.deploy}-ec2"
+  name = "${var.region}-${var.deploy}-${var.name}-ec2"
   roles = [ "${aws_iam_role.ec2.name}" ]
   policy_arn = "${aws_iam_policy.ec2.arn}"
 }
 
 resource "aws_iam_instance_profile" "ec2" {
-  name = "${var.region}-${var.deploy}-ec2"
+  name = "${var.region}-${var.deploy}-${var.name}-ec2"
+  path = "/instance/"
   roles = [ "${aws_iam_role.ec2.name}" ]
 }
